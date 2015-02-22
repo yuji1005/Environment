896cfffb508a460e410952b80c5eedbdcc99580f
yuji <yuji@haumea.solar.ueo.co.jp>
2015-02-23 00:23:51 +0900

    modify zsh script and README

--- zsh/install-antigen-prezto.sh
+++ zsh/install-antigen-prezto.sh
@@ -3,7 +3,7 @@
 # http://qiita.com/iri/items/2f9a39b9c47320f4c225
 #
 # Antigen を導入
-git clone https://github.com/zsh-users/antigen.git ~/projects/antigen
+#git clone https://github.com/zsh-users/antigen.git ~/projects/antigen
 source ~/projects/antigen/antigen.zsh
 
 # Antigen から Prezto を導入
@@ -17,5 +17,4 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-echo source /path/to/antigen/antigen.zsh > ~/.zshrc
-echo antigen bundle sorin-ionescu/prezto > ~/.zshrc
+echo -e "source ~/projects/antigen/antigen.zsh\\nantigen bundle sorin-ionescu/prezto" > ~/.zshrc
